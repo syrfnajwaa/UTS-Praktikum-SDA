45aa44ddac41f79d1b913a81b103c8659d8c179d
Mutia09 <cutmutia3564@gmail.com>
2025-03-22 22:17:32 +0700

    parsing_expression.c

--- parsing_expression.c
+++ parsing_expression.c
@@ -4,33 +4,33 @@
 
 typedef struct
 {
-    char data[100];
+    char data[100][100];
     int top;
 } Stack;
 
 void initStack(Stack *s) { s->top = -1; }
 int isEmpty(Stack *s) { return s->top == -1; }
-void push(Stack *s, char value)
+void push(Stack *s, char *value)
 {
     if (s->top < 100 - 1)
-        s->data[++(s->top)] = value;
+        strcpy(s->data[++(s->top)], value);
 }
-char pop(Stack *s)
+char *pop(Stack *s)
 {
     if (isEmpty(s))
     {
-        return '\0';
+        return "";
     }
     else
     {
         return s->data[(s->top)--];
     }
 }
-char peek(Stack *s)
+char *peek(Stack *s)
 {
     if (isEmpty(s))
     {
-        return '\0';
+        return "";
     }
     else
     {
@@ -77,7 +77,7 @@ int isCloseParent(char c)
     return (c == ')');
 }
 
-// fungsi untuk merubah infix menjadi prefix
+// fungsi untuk mengubah infix menjadi prefix
 void intopost()
 {
     printf("==================================================\n");
@@ -102,57 +102,169 @@ void intopost()
 
         else if (isOperator(index))
         {
-            while (!isEmpty(&s) && precedence(peek(&s)) >= precedence(index))
+            while (!isEmpty(&s) && precedence(peek(&s)[0]) >= precedence(index))
             {
-                char poped = pop(&s);
+                char poped = pop(&s)[0];
                 char temp[2] = {poped, '\0'};
                 strcat(postfix, temp);
             }
-            push(&s, index);
+            char temp[2] = {index, '\0'};
+            push(&s, temp);
         }
 
         else if (isOpenParent(index))
         {
-            push(&s, index);
+            char temp[2] = {index, '\0'};
+            push(&s, temp);
         }
 
         else if (isCloseParent(index))
         {
-            while (!isEmpty(&s) && !isOpenParent(peek(&s)))
+            while (!isEmpty(&s) && !isOpenParent(peek(&s) [0]))
             {
-                char poped = pop(&s);
-                char temp[2] = {poped, '\0'};
-                strcat(postfix, temp);
+                char *poped = pop(&s);
+                char temp[100];
+                strcat(temp, poped);
             }
             pop(&s);
         }
 
-        // print postfix and stack
+        // print postfix dan stack
         printf("Postfix: %s\n", postfix);
-        printf("Stack: %s\n", s.data);
+        printf("Stack Top: %s\n", peek(&s));
     }
 
     while (!isEmpty(&s))
     {
-        char poped = pop(&s);
-        char temp[2] = {poped, '\0'};
+        char *poped = pop(&s);
+        char temp[100];
+        strcpy(temp, poped);
         strcat(postfix, temp);
     }
 
     printf("Hasil konversi infix ke prefix: %s\n", postfix);
 }
 
-// //
-// void posttoin();
+// // Fungsi Postfix ke Infix
+void posttoin()
+{
+    printf("==================================================\n");
+    printf("=         Konversi Postfix ke Infix              =\n");
+    printf("==================================================\n");
+    printf("Masukkan ekspresi postfix: ");
+    char postfix[100];
+    fgets(postfix, sizeof(postfix), stdin);
+    postfix[strcspn(postfix, "\n")] = 0; 
+
+    Stack s;
+    initStack(&s);
+
+    for (int i = 0; i < strlen(postfix); i++)
+    {
+        char index = postfix[i];
+        if (isOperand(index))
+        {
+            char temp[2] = {index, '\0'};
+            push(&s, temp);
+        }
+        
+        else 
+        {
+            char op1[100], op2[100], exp[100];
+            strcpy(op1, pop(&s));
+            strcpy(op2, pop(&s));
+            sprintf(exp, "(%s%c%s)", op2, index, op1);
+            push(&s, exp);
+        }
+
+        printf("Stack Top: %s\n", isEmpty(&s) ? "" : s.data[s.top]);
+    }
+    printf("Hasil konversi postfix ke infix: %s\n", pop(&s));
+}
+
 
 // //
 // void intopre();
 
-// //
-// void pretoin();
+// // Fungsi Prefix ke Infix
+void pretoin()
+{
+    printf("==================================================\n");
+    printf("=         Konversi Prefix ke Infix               =\n");
+    printf("==================================================\n");
+    printf("Masukkan ekspresi prefix: ");
+    char prefix[100];
+    fgets(prefix, sizeof(prefix), stdin);
+    prefix[strcspn(prefix, "\n")] = 0; 
 
-// //
-// void pretopost();
+    Stack s;
+    initStack(&s);
+    int len = strlen(prefix);
+
+    for (int i = len - 1; i >= 0; i--) 
+    {
+        char index = prefix[i];
+
+        if (isOperand(index)) 
+        {
+            char temp[2] = {index, '\0'};
+            push(&s, temp);
+        }
+        else if (isOperator(index)) 
+        {
+            char op1[100], op2[100], exp[100];
+            strcpy(op1, pop(&s));
+            strcpy(op2, pop(&s));
+            sprintf(exp, "(%s%c%s)", op1, index, op2);
+            push(&s, exp);
+        }
+
+        printf("Stack Top: %s\n", isEmpty(&s) ? "" : s.data[s.top]); 
+    }
+
+    printf("Hasil konversi prefix ke infix: %s\n", pop(&s));
+}
+
+
+// // Fungsi Prefix ke Postfix
+void pretopost()
+{
+    printf("==================================================\n");
+    printf("=         Konversi Prefix ke Postfix             =\n");
+    printf("==================================================\n");
+    printf("Masukkan ekspresi prefix: ");
+    char prefix[100];
+    fgets(prefix, sizeof(prefix), stdin);
+    prefix[strcspn(prefix, "\n")] = 0; 
+
+    Stack s;
+    initStack(&s);
+    int len = strlen(prefix);
+
+    for (int i = len - 1; i >= 0; i--) 
+    {
+        char index = prefix[i];
+
+        if (isOperand(index)) 
+        {
+            char temp[2] = {index, '\0'};
+            push(&s, temp);
+        }
+        else if (isOperator(index)) 
+        {
+            char op1[100], op2[100], exp[100];
+            strcpy(op1, pop(&s));
+            strcpy(op2, pop(&s));
+            sprintf(exp, "%s%s%c", op1, op2, index);
+            push(&s, exp);
+        }
+
+        printf("Stack Top: %s\n", isEmpty(&s) ? "" : s.data[s.top]); 
+    }
+
+    printf("Hasil konversi prefix ke postfix: %s\n", pop(&s));
+}
+
 
 // //
 // void posttopre();
@@ -165,7 +277,11 @@ int main()
     printf("=         Tekan ENTER untuk Melanjutkan          =\n");
     printf("==================================================\n");
     getchar();
-    system("cls");
+    #ifdef _WIN32
+       system("cls");
+    #else
+       system("clear");
+    #endif
 
     printf("==================================================\n");
     printf("=  Silahkan pilih menu konversi yang diinginkan  =\n");
@@ -181,25 +297,29 @@ int main()
     int opsi;
     scanf("%d", &opsi);
     getchar();
-    system("cls");
+    #ifdef _WIN32
+       system("cls");
+    #else
+       system("clear");
+    #endif
 
     switch (opsi)
     {
     case 1:
         intopost();
         break;
-    // case 2:
-    //     posttoin();
-    //     break;
+    case 2:
+        posttoin();
+        break;
     // case 3:
     //     intopre();
     //     break;
-    // case 4:
-    //     pretoin();
-    //     break;
-    // case 5:
-    //     pretopost();
-    //     break;
+    case 4:
+        pretoin();
+        break;
+    case 5:
+        pretopost();
+        break;
     // case 6:
     //     posttopre();
     //     break;
